f52b6af9bbfd59a42006731178e56a82402efc2b
giampaolo <giampaolo.trapasso@gmail.com>
2015-11-29 22:43:58 +0100

    First commit

---
+++ src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,9 @@
+import it.cosenonjaviste.restapp._
+import org.scalatra._
+import javax.servlet.ServletContext
+
+class ScalatraBootstrap extends LifeCycle {
+  override def init(context: ServletContext) {
+    context.mount(new MyScalatraServlet, "/*")
+  }
+}
