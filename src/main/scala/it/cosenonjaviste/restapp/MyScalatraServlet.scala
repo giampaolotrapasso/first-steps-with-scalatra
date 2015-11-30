bff6e483c7ae78e4bb0c83761129c6c6171e22b6
giampaolo <giampaolo.trapasso@gmail.com>
2015-11-30 21:28:44 +0100

    Simplified message

--- src/main/scala/it/cosenonjaviste/restapp/MyScalatraServlet.scala
+++ src/main/scala/it/cosenonjaviste/restapp/MyScalatraServlet.scala
@@ -6,11 +6,9 @@ import scalate.ScalateSupport
 class MyScalatraServlet extends FirstStepsWithScalatraStack {
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-      </body>
-    </html>
+    "Hello world!"
   }
-
+  
 }
+
+
