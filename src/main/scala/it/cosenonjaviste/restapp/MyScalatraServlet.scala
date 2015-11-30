4fbe8e4506152768c6fc676c0ffcc4cd75429a99
giampaolo <giampaolo.trapasso@gmail.com>
2015-11-30 21:25:41 +0100

    Simplified Hello World

--- src/main/scala/it/cosenonjaviste/restapp/MyScalatraServlet.scala
+++ src/main/scala/it/cosenonjaviste/restapp/MyScalatraServlet.scala
@@ -9,7 +9,6 @@ class MyScalatraServlet extends FirstStepsWithScalatraStack {
     <html>
       <body>
         <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
       </body>
     </html>
   }
